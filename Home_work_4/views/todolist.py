1141a1907b1accf96cbaed2babf8dffd466af405
hitman <hitman47th@gmail.com>
2023-04-07 08:41:13 +0300

    add feature done task

--- Home_work_4/views/todolist.py
+++ Home_work_4/views/todolist.py
@@ -1,34 +1,52 @@
 from flask import Blueprint, render_template, request, jsonify
 from werkzeug.exceptions import BadRequest
 
+from dataclasses import dataclass
+
 todo_app = Blueprint("product_app", __name__)
 
+@dataclass
+class Task:
+    name: str
+    isDone: bool
+    # priority: int
 
-PRODUCTS = {
-    1: 'Smartphone',
-    2: 'Tablet',
-    3: 'Laptop',
+    def update_status(self, isDone:bool):
+        self.isDone = isDone
+
+TASKS = {
+    1: Task('Clean a home', False),
+    2: Task('Learn Python', False),
+    3: Task('Buy purchase', True)
 }
+SEQ = len(TASKS)
 
-
-@todo_app.route("/")
+@todo_app.route("/", methods=['GET', 'POST'])
 def product_list():
-    return render_template("todolist/index.html", products=PRODUCTS)
+    global SEQ
+    if request.method == 'POST':
+        print(request.form['data'])
+        SEQ += 1
+        req_data = request.form['data']
+        TASKS[SEQ] = Task(req_data, False)
+    return render_template("todolist/index.html", tasks=TASKS)
 
 
-@todo_app.route("/<int:product_id>/", methods=['GET', 'DELETE'])
-def product_detail(product_id: int):
+@todo_app.route("/<int:task_id>/", methods=['GET','DELETE', 'POST'])
+def task_detail(task_id: int):
     try:
-        product_name = PRODUCTS[product_id]
+        task_name = TASKS[task_id]
     except KeyError:
-        raise BadRequest(f"Invalid product id #{product_id}")
+        raise BadRequest(f"Invalid product id #{task_id}")
 
     if request.method == 'DELETE':
-        PRODUCTS.pop(product_id)
+        TASKS.pop(task_id)
         return jsonify(ok=True)
 
-    return render_template(
-        "products/detail.html",
-        product_id=product_id,
-        product_name=product_name,
-    )
+    if request.method == 'POST':
+        isDone = True if request.form['isDone'] == '1' else False
+        print('isDone=',isDone)
+        TASKS[task_id].update_status(isDone)
+        print(TASKS[task_id])
+
+    return TASKS[task_id].name
